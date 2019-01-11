559425906334a42fd4696f557b31b550b76aa9c7
Chris Kakos <ckakos@gmail.com>
2019-01-11 16:27:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -9,11 +9,11 @@ class Shoe
   def initialize(brand)
       @brand = brand
 
-      if BRANDS.include?(brand)
-        BRANDS << brand
-      end
-
-      # BRANDS << brand unless BRANDS.include?(brand)
+      # if BRANDS.include?(brand)
+      #   BRANDS << brand
+      # end
+      
+      BRANDS << brand unless BRANDS.include?(brand)
   end
 
 
