a48236ed79a27445a23ab4f5052cc748cf15b56e
Chris Kakos <ckakos@gmail.com>
2019-01-11 03:58:58 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,25 @@
+require 'pry'
+
 class Shoe
-  attr_accessor :color, :size, :material, :condition
+  attr_accessor :color, :size, :material, :condition,
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
-    @brand = brand
+      @brand = brand
+  end
+
+  #Setter
+  def brands=(brands)
+    @brands = brands
+    if brands.include?(brand)
+      BRANDS << brand
+    end
+  end
+
+  def brands
+    @brands
   end
 
   def cobble
